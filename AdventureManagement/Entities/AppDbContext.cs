8fb9386adb2559353bdfd1b7067335b5ab74c1db
Nguyen Le Hong Giang <giangnlh.forworking@gmail.com>
2024-09-12 21:33:10 +0700

    Update AppDbContext

--- AdventureManagement/Entities/AppDbContext.cs
+++ AdventureManagement/Entities/AppDbContext.cs
@@ -23,76 +23,8 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<ParticipantInteraction> ParticipantInteractions { get; set; }
 
+    public virtual DbSet<AdventureOrganism> AdventureOrganisms { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlServer("Server=powa;Database=AdventureManagement;Trusted_Connection=True;TrustServerCertificate=True");
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Adventure>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__Adventur__3214EC073517FE5D");
-
-            entity.Property(e => e.CreatedAt)
-                .HasDefaultValueSql("(getdate())")
-                .HasColumnType("datetime");
-            entity.Property(e => e.Location).HasMaxLength(100);
-            entity.Property(e => e.Title).HasMaxLength(100);
-
-            entity.HasOne(d => d.Guide).WithMany(p => p.Adventures)
-                .HasForeignKey(d => d.GuideId)
-                .HasConstraintName("FK__Adventure__Guide__46E78A0C");
-        });
-
-        modelBuilder.Entity<Guide>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__Guides__3214EC0725D46CFD");
-
-            entity.Property(e => e.Expertise).HasMaxLength(100);
-            entity.Property(e => e.Name).HasMaxLength(100);
-        });
-
-        modelBuilder.Entity<Organism>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__Organism__3214EC079528CECD");
-
-            entity.Property(e => e.Habitat).HasMaxLength(100);
-            entity.Property(e => e.Name).HasMaxLength(100);
-        });
-
-        modelBuilder.Entity<Participant>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__Particip__3214EC0792E0B546");
-
-            entity.HasIndex(e => e.Email, "UQ__Particip__A9D10534A5091FE0").IsUnique();
-
-            entity.Property(e => e.CreatedAt)
-                .HasDefaultValueSql("(getdate())")
-                .HasColumnType("datetime");
-            entity.Property(e => e.Email).HasMaxLength(100);
-            entity.Property(e => e.Name).HasMaxLength(100);
-        });
-
-        modelBuilder.Entity<ParticipantInteraction>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__Particip__3214EC07CD909766");
-
-            entity.Property(e => e.CreatedAt)
-                .HasDefaultValueSql("(getdate())")
-                .HasColumnType("datetime");
-
-            entity.HasOne(d => d.Adventure).WithMany(p => p.ParticipantInteractions)
-                .HasForeignKey(d => d.AdventureId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Participa__Adven__4E88ABD4");
-
-            entity.HasOne(d => d.Participant).WithMany(p => p.ParticipantInteractions)
-                .HasForeignKey(d => d.ParticipantId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Participa__Parti__4F7CD00D");
-        });
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
